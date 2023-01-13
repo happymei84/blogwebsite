a37211e505dfa21a6168a34bef1780005ee2f463
TWDANNO220706\oem <a100st61226@gmail.com>
2023-01-13 11:32:01 +0800

    searchfunction

--- src/component/Search.js
+++ src/component/Search.js
@@ -3,41 +3,22 @@ import "../style/_search.css";
 import { useNavigate } from "react-router-dom";
 
 const Search = () => {
-  let [postTitle, setinput] = useState("");
+  let [searchValue, setinput] = useState("");
   const inputhandler = (e) => {
     setinput(e.target.value);
   };
   let navigate = useNavigate();
 
   const searchEngine = (e) => {
-    e.preventDefault();
-    fetch("https://localhost:7175/Home/SearchFunction?model=" + postTitle, {
-      //直接使用url?model= 回傳一個string
-      method: "POST",
-      // body: JSON.stringify({ //用body:JSON.stringify只能回傳obj
-      //   PostTitle:postTitle,
-      // }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.susseccStatus === true) {
-          alert("找到了!");
-          navigate("/Searchingpage",{state:{returnData:data.returnData}})
-        
-        } else if (data.susseccStatus === false) {
-          alert("沒找到~");
-        }
-      })
-      .catch((error) => console.error(error));
+    e.preventDefault();   
+    navigate("/Searchingpage", {state:{searchValue:searchValue}});
+    console.log("searchValue"+ searchValue)
   };
 
   return (
     <div>
       <form onSubmit={searchEngine}>
-        <input onChange={inputhandler} value={postTitle} />
+        <input onChange={inputhandler} value={searchValue} />
         <button>Search</button>
       </form>
     </div>
--- src/pages/Searchingpage.js
+++ src/pages/Searchingpage.js
@@ -2,27 +2,50 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import PostContainer from "../component/PostContainer";
 
-
-
 const Searchingpage = () => {
-    const location = useLocation([]);
-    const { returnData } = location.state;
+  const location = useLocation([]);
+  const { searchValue } = location.state;
   let [inputdata, setdata] = useState([]); //記值的地方
-  
-  useEffect(()=>{
 
-  },)
-    console.log(location.state)
-    
+  useEffect(() => {
+    fetch("https://localhost:7175/Home/SearchFunction?model=" + searchValue, {
+      //直接使用url?model= 回傳一個string
+      method: "POST",
+      // body: JSON.stringify({ //用body:JSON.stringify只能回傳obj
+      //   PostTitle:postTitle,
+      // }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.susseccStatus === true) {
+          // alert("找到了!");
+          var allindex = [];
+          data.returnData.forEach((index) => {
+            //javascrip 迴圈(item是array的第幾項，index是值)
+            // console.log(index);
+            allindex = allindex.concat(index); //array相加(allindex[]+index[])
+            console.log(allindex.concat(index));
+          });
+          setdata(allindex);
+        } else if (data.susseccStatus === false) {
+          alert("沒找到~");
+        }
+      })
+      .catch((error) => console.error(error));
+  }, [searchValue]);
 
   return (
     <div>
-        
-         <PostContainer inputData={location.state} />
-       
-      
+      {inputdata.map((data) => (
+        <>
+          <PostContainer inputData={data} />
+        </>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Searchingpage
+export default Searchingpage;
